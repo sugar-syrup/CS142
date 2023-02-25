052d1bbe7338b6054e444475bb0b6d296f304a89
sugar-syrup <sugarsyrup99@gmail.com>
2023-02-26 02:58:18 +0530

    first commit

---
+++ assignment1.py
@@ -0,0 +1,129 @@
+#[Problem 1]
+def ntom(n,m,mult_counter=0): #mult_counter calculates the number of multiplications used
+    if m==1:       #when m is 1
+        print(f"{mult_counter} multiplications were used")
+        return n 
+    elif m % 2==0: # when m is even
+        mult_counter += 1
+        return ntom(n**2,m/2,mult_counter)
+    else:          # m is odd
+        mult_counter += 2
+        return n*(ntom(n**2,(m-1)/2,mult_counter))
+
+print("[Problem 1]: ")
+print(ntom(2,61))
+
+#[Problem 2]
+
+#[Part 1]
+def SingleCourse(List):
+    courses = {}
+    for i in List:                                  #O(n)
+        if i in courses:
+            courses[i] += 1                         #O(1)
+        else:
+            courses[i] = 1                          #0(1)
+    for key,value in courses.items():               #O(n)
+        if value == 1:
+            print(f"{key} have only {value} hour!") #O(1)
+
+print("[Problem 2(a)]")    
+SingleCourse(["B101","CS141","M102","C102","P102","C102","P102","M102","B101"])
+
+#[Part 2]
+def findSingle(List):
+    Sum = ((len(List)+1)*(len(List)+3))//4 #Sum of 2n integers
+    for i in List: 
+        Sum -= i
+    #After the loop has run, the only part of sum remaining will be that one missing number
+    print(Sum)
+
+print("[Problem 2(b)]")
+findSingle([1,1,2,2,3,4,4,5,5,6,7,6,7])
+    
+#[Problem 3]
+
+#[Part 1]
+def ThisSort1(List,k):
+    aux1 = [i for i in range(k-1,len(List),k)]            #Indices of multiples in the list
+    aux2 = [i for i in range(len(List)) if i not in aux1] #Indices of non-multiples in the list
+
+    #Sorting in ascending order
+    #Considers only elements with index in aux1
+    for i in range(len(aux1)):                            
+        current = List[aux1[i]]
+        j = i - 1
+        while j>=0 and current < List[aux1[j]]:
+            List[aux1[j+1]] = List[aux1[j]]
+            j -= 1
+        List[aux1[j+1]] = current
+
+    #Sorting in descending order
+    #Considers only elements with index in aux2
+    for i in range(len(aux2)):
+        current = List[aux2[i]]
+        j = i-1
+        while j>=0 and current > List[aux2[j]]:
+            List[aux2[j+1]] = List[aux2[j]]
+            j -= 1
+        List[aux2[j+1]] = current
+    return List
+
+print("Problem 3(a)")
+print(ThisSort1([31,12,21,55,14,1,51,30,2,7],3))
+
+
+#[Part 2]
+def ThisSort(List,k):
+    
+    #Sorting the elements at nth position in Ascending order
+    for i in range(k-1,len(List),k):
+        current = List[i]
+        j = i-k
+        while j>=0 and List[j] > current:
+            List[j+k] = List[j]
+            j-=k
+        List[j+k]=current
+    
+    #Sorting rest of the elements in Descending order
+    for i in range(len(List)):
+        if (i+1)%k!=0:          #Discards elements with k-divisible position
+            current = List[i]
+            if i%k == 0:
+                j = i-2
+            else:
+                j = i-1
+            while j>=0 and List[j]<current:
+                if (j+2)%k==0:
+                    List[j+2]=List[j]
+                else:
+                    List[j+1]=List[j]
+                if j%k == 0:
+                    j-=2
+                else:
+                    j-=1
+            if (j+2)%k == 0:
+                List[j+2]=current
+            else:
+                List[j+1]=current
+    return List    
+    
+print("[Problem 3(b)]")
+print(ThisSort([31,12,21,55,22,1,51,30,2,7],3))
+
+
+#[Problem 4]
+def inversion(List):
+    count = 0  #Counts the number of inversion pairs
+    for i in range(1,len(List)):
+        current = List[i]
+        j = i-1
+        while j>=0 and current<List[j]:  #loop runs when List[i]<List[j]. i>j since j = j-1 => inversion pair
+            List[j+1] = List[j]
+            count += 1                   #Adds the count whenever loop is run
+            j -= 1
+        List[j+1] = current
+    print(f"The List has {count} inversion pairs!")
+print("[Problem 4]: ")
+inversion([1,3,2,9,5,6,2,21,11])
+
---
+++ assignment3.py
@@ -0,0 +1,109 @@
+#[Problem 1]
+def find_pop(expression):
+    count  = 0
+    flag = True
+    for i in range(len(expression)):
+        if expression[i] == "(":
+            count += 1
+        elif expression[i] == ")":
+            count -= 1
+        if count == 1 and not flag:
+            return i+1
+            break
+        flag = False
+
+def evaluate(expression):
+    if len(expression) == 1:
+        return int(expression[0])
+    elif len(expression) == 5:
+        if expression[2] == "+":
+            return int(expression[1])+int(expression[3])
+        else:
+            return int(expression[1])-int(expression[3])
+    else:
+        a = find_pop(expression)
+        if expression[a] == "+":
+            return int(evaluate(expression[1:a])) + int(evaluate(expression[a+1:-1]))
+        else:
+            return int(evaluate(expression[1:a])) - int(evaluate(expression[a+1:-1]))
+            
+print("Problem 1")
+print(evaluate([*"((1+(((5+4)-1)+(2+5)))-(3+3))"]))
+
+
+#Problem 2
+List = [8, 4, 2, 1]
+count = 0
+def merge_sort(List):
+    if len(List) == 1:
+        return List
+    mid = len(List) // 2
+    left = merge_sort(List[:mid])
+    right = merge_sort(List[mid:])
+    return merge(left, right)
+def merge(left, right):
+    global count
+    merged_list = list()
+    while len(left) != 0 and len(right) != 0:
+        if left[0] <= right[0]:
+            merged_list.append(left.pop(0))
+        else:
+            count += len(left)
+            merged_list.append(right.pop(0))
+    while len(left) != 0:
+        merged_list.append(left.pop(0))
+    while len(right) != 0:
+        merged_list.append(right.pop(0))
+    return merged_list
+
+print("Problem 2")
+merge_sort(List)
+print(count)
+
+#Problem 3
+#Insertion sort, but using binary sort for comparing, rather than looping through every element
+def Search(Arr,target,left,right):
+    if left == right:
+        if Arr[left]<target:
+            return left+1
+        else:
+            return left
+    check = (left+right)//2
+    if Arr[check]>=target:
+        return Search(Arr,target,left,check)
+    else:
+        if check == left:
+            check = right
+        return Search(Arr, target, check, right)
+def insertionSort(List):
+    for i in range(1, len(List)):
+        current = List[i]
+        j = i - 1
+        List.pop(i)
+        index = Search(List[: j + 1], current, 0, len(List[: j + 1]) - 1)
+        List.insert(index, current)
+    return List
+a = [4,66,23,12,67,49,22,11]
+
+print("Problem 3")
+insertionSort(a)
+print(a)
+
+#Problem 4
+def find_median(arr1, arr2):
+    #Base condition
+    if len(arr1) == 2:
+        return (max(arr1[0], arr2[0]), min(arr1[1], arr2[1]))
+    right =  len(arr1) // 2 + len(arr1) % 2 - 1
+    left = len(arr1) // 2
+    if arr1[left] == arr2[right]:
+        return (arr1[left], arr1[left])
+    elif arr1[left] < arr2[right]:
+        return find_median(arr1[left:], arr2[: right + 1])
+    else:
+        return find_median(arr1[: left + 1], arr2[right:])
+        
+list1 = [3,5,6,7,9]
+list2 = [5,8,9,11,23]
+print("Problem 4")
+print(find_median(list1,list2))
