07c08fe486a8d3422d39259c66ba2605377713e4
sugar-syrup <sugarsyrup99@gmail.com>
2023-04-02 16:22:14 +0530

    assignment8 problem2 added

--- assignment8.py
+++ assignment8.py
@@ -35,3 +35,65 @@ node6.left = node9
 print("[Problem 1]")
 
 binTree.inorderPrint()
+print()
+
+# Problem 2
+
+def evaluate(BinTree,node):
+  if (node.left or node.right) == None:
+    return node.data
+
+  left = int(evaluate(BinTree,node.left))
+  right = int(evaluate(BinTree,node.right))
+
+  if node.data == "+":
+    return left + right
+  elif node.data == "-":
+    return left - right
+  elif node.data == "x":
+    return left * right
+  elif node.data == "/":
+    return left / right
+
+def GenerateTree(BinTree,Expression,node):
+  if node == None:
+    node = Node("None")
+    BinTree.root = node
+
+  if len(Expression) == 1:
+    node.data = (Expression[0])
+    return 
+
+  mid = findOperator(Expression)
+  node.data = Expression[mid] 
+  node.left = Node("None")   
+  node.right = Node("None")
+
+  GenerateTree(BinTree,Expression[1:mid],node.left)
+  GenerateTree(BinTree,Expression[mid+1:-1],node.right)
+
+def findOperator(Expression):
+  left = right = 0
+  for i in range(1,len(Expression)-1):
+    if Expression[i] == "(":
+      left += 1
+    if Expression[i] == ")":
+      right += 1
+    if left == right:
+      return i + 1
+  for i in range(len(Expression)):
+    if Expression[i] in '+-x/':
+      return i
+  return -1
+
+BinTree = Tree(None)
+
+print("[Problem 2]")
+
+GenerateTree(BinTree,[*"(((3+5)x7)+5)"],BinTree.root)
+
+print("Binary Tree: ",end="")
+BinTree.inorderPrint()
+print()
+
+print(f"Output - {evaluate(BinTree,BinTree.root)}")
